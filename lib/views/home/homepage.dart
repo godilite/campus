f266896da1abf85a26449d87bae1d84049627059
Godwin Asuquo <godilite@gmail.com>
2020-09-09 18:10:49 +0100

    campusel starts here

---
+++ lib/views/home/homepage.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class HomeView extends StatefulWidget {
+  @override
+  _HomeViewState createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints viewportConstraints) {
+          return SingleChildScrollView(
+            child: ConstrainedBox(
+              constraints:
+                  BoxConstraints(minHeight: viewportConstraints.maxHeight),
+              child: Stack(children: [
+                // remaining stuffs
+              ]),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
